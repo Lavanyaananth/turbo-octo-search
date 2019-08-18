4e3dc6bcb29c20c3664c0218ce0c44d087f4848c
lavanyaananth <lavanyaanand.11@gmail.com>
2019-08-18 23:49:22 +0530

    creating XHR

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("wiki search");
+
+    let xhr = new XMLHttpRequest;
+    xhr.open ('GET','https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=modi' , true);
+    xhr.onload = function() 
+    {
+        //check if the status is 200
+        if (this.status === 200) 
+            {
+                //return server response as an object 
+                console.log(JSON.parse(this.responseText));
+    }
+            }
+    //call send
+    xhr.send();
+
+
